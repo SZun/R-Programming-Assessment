85001a9ca3fde2759e4705b6909095e2616f081c
Sam Zun <zunsamuel97@gmail.com>
2023-02-03 15:38:54 -0600

    completed median imputation method

--- data_preparation.R
+++ data_preparation.R
@@ -16,8 +16,15 @@ display_data <- function(data) {
 get_xlsx_list <- function(file_name, read_method) {
   path <- paste(RAW_DATA_DIRECTORY,file_name,".xlsx",sep="")
   list <- read_excel(path)
+  list$Date <- as.Date(dates_col, format= "%Y-%m-%d")
   list[2:ncol(list)] <- lapply(list[2:ncol(list)],function(x) round(x,4))
   return(list)
+}
+
+median_imputation <- function(funds) {
+  funds[is.na(funds$Fund2), "Fund2"] <- median(funds[!is.na(funds$Fund2), "Fund2"][[1]])
+  funds[is.na(funds$Fund3), "Fund3"] <- median(funds[!is.na(funds$Fund3), "Fund3"][[1]])
+  return(funds)
 }
 
 get_factors <- function(dates) {
@@ -30,7 +37,7 @@ get_factors <- function(dates) {
 }
 
 funds <- get_xlsx_list("Funds")
-head(funds,10)
+funds <- median_imputation(funds)
 # display_data(funds)
 benchmark <- get_xlsx_list("Bmark")
 # display_data(benchmark)
