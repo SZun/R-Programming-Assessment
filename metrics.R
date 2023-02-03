22398dc021e9005af99431d1406518bc75ae1ba6
Sam Zun <zunsamuel97@gmail.com>
2023-02-03 16:35:48 -0600

    commented code and added method definitions

---
+++ metrics.R
@@ -0,0 +1,14 @@
+# Set working directory
+setwd("~/Documents/repos/R-Programming-Assessment/")
+
+# Method to get the annualized ex-post tracking error
+get_annualized_ex_post_tracking_error <- function() {}
+
+# Method to get annualized information ratio
+get_annualized_information_ratio <- function() {}
+
+# Method to get beta
+get_beta <- function() {}
+
+# Import dataset
+dataset <- read.csv("./data/dataset.csv")[2:10]
