b1d744c482d03215e3ae03f527ac453cd90a983b
Sam Zun <zunsamuel97@gmail.com>
2023-02-04 14:52:40 -0600

    saved model stats to dataframe

--- ff3f_model_linear_regression.R
+++ ff3f_model_linear_regression.R
@@ -4,64 +4,51 @@ setwd("~/Documents/repos/R-Programming-Assessment/")
 # Import dataset
 dataset <- read.csv("./data/dataset.csv")
 
-# Method to get linear regression from fund
-get_linear_regression <- function(fund, full_dataset=dataset) {
-  # Generate linear regression
-  lr <- lm(fund + Mkt.RF~SMB + HML + RF, data = full_dataset)
-  # Return linear regression
-  return(lr)
-}
-# Method to calculate annualized alpha from linear regression
-calculate_annualized_alpha <- function(linear_regression) {
-  # Calculate annualized alpha
-  alpha <- linear_regression$coefficients[[1]] * 12
-  # Return alpha
-  return(alpha)
-}
+# Create empty model statistics data.frame
+model_stats_df = data.frame(data.frame(matrix(ncol = 10, nrow = 3)))
 
-# Method to extract alpha t stat
-get_alpha_t_stat <- function(linear_regression) {
-  # Extract alpha t-stat
-  alpha_t_stat <- summary(linear_regression)$coefficients[1,3]
-  # Return alpha t-stat
-  return(alpha_t_stat)
-}
+# Vector of column names
+column_names <- c("Fund Name", 
+                  "Alpha (annualized)",
+                  "Alpha t-Stat", 
+                  "Developed Market Beta", 
+                  "Developed Market t-Stat", 
+                  "Developed Size Beta", 
+                  "Developed Size t-Stat", 
+                  "Developed Value Beta", 
+                  "Developed Value t-Stat", 
+                  "Adj. R2")
+# Set column names
+colnames(model_stats_df) <- column_names
 
-# Method to calculate developed market beta
-calculate_developed_market_beta <- function() {}
+# Vector to hold funds
+funds <- c(dataset$Fund1,dataset$Fund2,dataset$Fund3)
 
-# Method to calculate developed market t stat
-calculate_developed_market_t_stat <- function() {}
-
-# Method to calculate developed size beta
-calculate_developed_size_beta <- function() {}
-
-# Method to calculate developed size t stat
-calculate_developed_size_t_stat <- function() {}
-
-# Method to calculate developed value beta
-calculate_developed_value_beta <- function() {}
-
-# Method to calculate developed value t stat
-calculate_developed_value_t_stat <- function() {}
-
-# Method to get the adjusted r squared value from the linear regression
-get_adjusted_r_squared <- function(linear_regression) {
-  # Get Adjusted R Squared
-  adj_r2 <- summary(linear_regression)$adj.r.squared
-  # Return Adjusted R Squared
-  return(adj_r2)
+# Loop over number of rows in dataframe
+for(i in 1:nrow(model_stats_df)){
+  # Save linear regression
+  lr <- lm(funds[[i]] + RF~Mkt.RF + SMB + HML, data = dataset)
+  # Save linear regression summary
+  lr_summary <- summary(lr)
+  # Save alpha, beta and t stats
+  alpha_beta_t_stat <- lr_summary$coefficients[,c(1,3)]
+  
+  # Set values
+  model_stats_df[i,column_names[1]] = c("Fund 1", "Fund 2", "Fund 3")[i]
+  model_stats_df[i,column_names[2]] = alpha_beta_t_stat[1,1] * 12
+  model_stats_df[i,column_names[3]] = alpha_beta_t_stat[1,2]
+  model_stats_df[i,column_names[4]] = alpha_beta_t_stat[2,1]
+  model_stats_df[i,column_names[5]] = alpha_beta_t_stat[2,2]
+  model_stats_df[i,column_names[6]] = alpha_beta_t_stat[3,1]
+  model_stats_df[i,column_names[7]] = alpha_beta_t_stat[3,2]
+  model_stats_df[i,column_names[8]] = alpha_beta_t_stat[4,1]
+  model_stats_df[i,column_names[9]] = alpha_beta_t_stat[4,2]
+  model_stats_df[i,column_names[10]] = lr_summary$adj.r.squared
 }
 
-# Method to get data.frame of the models statistics
-get_model_statistics_df <- function() {}
+# Round values
+model_stats_df[2:9] <- as.data.frame(lapply(model_stats_df[,2:9],function(x) round(x,4)))
+model_stats_df[10] <- round(model_stats_df[10],5)
 
-# Save model statistics data.frame
-model_stats_df <- get_model_statistics_df()
 # Display data.frame
 model_stats_df
-
-# Scratch
-lr <- get_linear_regression(dataset$Fund1)
-summary(lr)
-get_alpha_t_stat(lr)
