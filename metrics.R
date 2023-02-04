fc5b1da7796fd00aa32208816983b419ab6232e6
Sam Zun <zunsamuel97@gmail.com>
2023-02-03 21:27:40 -0600

    created dataframe of metrics

--- metrics.R
+++ metrics.R
@@ -9,7 +9,7 @@ BENCHMARK <- dataset$Bmark
 # Method to calculate the annualized ex-post tracking error
 calculate_annualized_ex_post_tracking_error <- function(fund,benchmark=BENCHMARK) {
   # Calculate tracking error
-  annualized_ex_post_tracking_error <- sd(fund-benchmark) * 12
+  annualized_ex_post_tracking_error <- sd(fund-benchmark) * 1.2
   # Return tracking error
   return(annualized_ex_post_tracking_error)
 }
@@ -30,16 +30,17 @@ calculate_beta <- function(fund,benchmark=BENCHMARK) {
   return(beta)
 }
 
-# get_fund_metrics <- function(fund) {
-#   tracking_error <- get_annualized_ex_post_tracking_error(funds)
-#   information_ratio <- get_annualized_information_ratio(funds)
-#   beta <- get_beta(funds)
-#   fund_metrics = c(Tracking_Error=tracking_error,Information_Ratio=information_ratio,Beta=beta)
-#   return(fund_metrics)
-# }
-# 
-# fund1_metrics <- get_fund_metrics(dataset$Fund1)
-# fund2_metrics <- get_fund_metrics(dataset$Fund2)
-# fund3_metrics <- get_fund_metrics(dataset$Fund3)
-# metrics <- as.data.frame(Fund1=fund1_metrics,Fund2=fund2_metrics,Fund1=fund3_metrics)
-# metrics
+get_fund_metrics <- function(fund) {
+  tracking_error <- calculate_annualized_ex_post_tracking_error(fund)
+  information_ratio <- calculate_annualized_information_ratio(fund)
+  beta <- calculate_beta(fund)
+  fund_metrics = c("Tracking Error"=tracking_error,"Information Ratio"=information_ratio,Beta=beta)
+  return(fund_metrics)
+}
+
+metrics <- data.frame(
+    Fund1=get_fund_metrics(dataset$Fund1),
+    Fund2=get_fund_metrics(dataset$Fund2),
+    Fund3=get_fund_metrics(dataset$Fund3)
+  )
+metrics
