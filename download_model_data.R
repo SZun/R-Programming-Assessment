36c8716fb217300c4193d58a51a290fea28c371c
Sam Zun <zunsamuel97@gmail.com>
2023-02-04 15:20:57 -0600

    cleaned up script for downloading and extracting data

--- download_model_data.R
+++ download_model_data.R
@@ -10,7 +10,7 @@ setwd("~/Documents/repos/R-Programming-Assessment/data/raw_data/")
 download_url = paste("http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/",
                      "F-F_Research_Data_Factors_CSV",
                      ".zip",sep="")
-zip_path <- paste(getwd(),"F-F_Research_Data_Factors_CSV",".zip",sep="")
+zip_path <- paste(getwd(),"/F-F_Research_Data_Factors_CSV",".zip",sep="")
 
 # Download .zip
 wget(download_url)
